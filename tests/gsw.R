66678455072735aad8bf7f8586502670a30dc5f3
Dan Kelley <kelley.dan@gmail.com>
2014-12-22 10:56:18 -0400

    digits

--- tests/gsw.R
+++ tests/gsw.R
@@ -134,9 +134,10 @@ SA <- c(34.7118, 34.8915, 35.0256, 34.8472, 34.7366, 34.7324)
 p <- c(      10,      50,     125,     250,     600,    1000)
 saturation_fraction <- 1
 tf <- gsw_t_freezing(SA, p, saturation_fraction)
-print("Cancelled gsw_t_freezing() test because it clearly fails:\n")
+options(digits=14)
 print(data.frame(gsw=tf, expect=c(-1.902730710149803, -1.942908619287183, -2.006861069199743,
                                   -2.090985086875259, -2.351293130342102, -2.660498762776720)))
+print("Cancelled gsw_t_freezing() test because it clearly fails:\n")
 #stopifnot(all.equal(tf, c(-1.902730710149803, -1.942908619287183, -2.006861069199743,
 #                          -2.090985086875259, -2.351293130342102, -2.660498762776720)))
 
