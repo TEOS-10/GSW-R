b5257e27feb86b88eb82d75def6bf5a1ed503e17
Dan Kelley <kelley.dan@gmail.com>
2015-09-12 12:22:09 -0300

    gsw_cp_t_exact() updated

--- R/gsw.R
+++ R/gsw.R
@@ -409,9 +409,16 @@ gsw_C_from_SP <- function(SP, t, p)
 #' @param p sea pressure [ dbar ]
 #' @return heat capacity [ J/(kg*K) ]
 #' @examples 
-#' gsw_cp_t_exact(34.7118, 28.7856, 10) # 4002.888003958537
+#' library(testthat)
+#' SA <- c(34.7118, 34.8915, 35.0256, 34.8472, 34.7366, 34.7324)
+#' t <-  c(28.7856, 28.4329, 22.8103, 10.2600,  6.8863,  4.4036)
+#' p <-  c(     10,      50,     125,     250,     600,    1000)
+#' cp_t_exact <- gsw_cp_t_exact(SA, t, p)
+#' expect_equal(cp_t_exact, 1e3*c(4.002888003958537, 4.000980283927373, 3.995546468894633,
+#'                                3.985076769021370, 3.973593843482723, 3.960184084786622))
+#' 
 #' @references
-#' \url{http://www.teos-10.org/pubs/gsw/v3_04/html/gsw_cp_t_exact.html}
+#' \url{http://www.teos-10.org/pubs/gsw/html/gsw_cp_t_exact.html}
 gsw_cp_t_exact <- function(SA, t, p)
 {
     l <- argfix(list(SA=SA, t=t, p=p))
