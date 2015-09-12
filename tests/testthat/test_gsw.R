d43fd620822fc91424d7ad61608fdfc45b645b27
Dan Kelley <kelley.dan@gmail.com>
2015-09-12 09:44:52 -0300

    trial test-build (adiabatic lapse)

---
+++ tests/testthat/test_gsw.R
@@ -0,0 +1,14 @@
+library(gsw)
+# Test against with values provided on the TEOS-10 website, e.g.
+#   http://www.teos-10.org/pubs/gsw/html/gsw_adiabatic_lapse_rate_from_t.html
+# for the first function.
+
+## gsw_adiabatic_lapse_rate_from_CT()
+SA <- c(34.7118, 34.8915, 35.0256, 34.8472, 34.7366, 34.7324)
+CT <- c(28.7856, 28.4329, 22.8103, 10.2600,  6.8863,  4.4036)
+p <- c(      10,      50,     125,     250,     600,    1000)
+lr <- gsw_adiabatic_lapse_rate_from_CT(SA, CT, p)
+expect_equal(lr, 1e-7*c(0.240199646230069, 0.238457486976761, 0.203635157319712,
+                        0.119829566859790, 0.100052760967308, 0.087773070307283))
+
+
