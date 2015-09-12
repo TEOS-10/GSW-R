15358afae7a019794c9111477e7846bdcc902219
Dan Kelley <kelley.dan@gmail.com>
2015-09-12 19:32:56 -0300

    gsw_SA_from_Sstar() updated

--- R/gsw.R
+++ R/gsw.R
@@ -1285,10 +1285,17 @@ gsw_SA_from_SP <- function(SP, p, longitude, latitude)
 #' @param latitude latitude in decimal degrees [ -90 to 90 ]
 #' @return Absolute Salinity [ g/kg ]
 #' @examples
-#' gsw_SA_from_Sstar(34.7115, 10, 188, 4) # 34.711724663585905
+#' library(testthat)
+#' SP <- c(34.7115, 34.8912, 35.0247, 34.8436, 34.7291, 34.7197)
+#' p <- c(      10,      50,     125,     250,     600,    1000)
+#' lat <- c(     4,       4,       4,       4,       4,       4)
+#' long <- c(  188,     188,     188,     188,     188,     188)
+#' SA <- gsw_SA_from_Sstar(SP, p, long, lat)
+#' expect_equal(SA, c(34.711724663585905, 34.891561223296009, 35.025594598699882,
+#'                    34.847235885385913, 34.736694493054166, 34.732387111902753))
 #' @seealso \code{\link{gsw_Sstar_from_SA}} does the reverse.
 #' @references
-#' \url{http://www.teos-10.org/pubs/gsw/v3_04/html/gsw_SA_from_Sstar.html}
+#' \url{http://www.teos-10.org/pubs/gsw/html/gsw_SA_from_Sstar.html}
 gsw_SA_from_Sstar <- function(Sstar, p, longitude, latitude)
 {
     if (missing(longitude)) stop("must supply longitude")
