5ab064f19b05cd2a6597e37f222e907c9fd09d57
Dan Kelley <kelley.dan@gmail.com>
2015-09-12 11:36:26 -0300

    gsw_alpha_on_beta() works

--- R/gsw.R
+++ R/gsw.R
@@ -226,10 +226,16 @@ gsw_alpha <- function(SA, CT, p)
 #' @param p sea pressure [ dbar ]
 #' @return ratio of thermal expansion coefficient to haline contraction coefficient [ (g/kg)/K ]
 #' @examples
-#' gsw_alpha_on_beta(34.7118, 28.8099, 10) # 0.452454540612631
+#' library(testthat)
+#' SA <- c(34.7118, 34.8915, 35.0256, 34.8472, 34.7366, 34.7324)
+#' CT <- c(28.8099, 28.4392, 22.7862, 10.2262,  6.8272,  4.3236)
+#' p <-  c(     10,      50,     125,     250,     600,    1000)
+#' alpha_on_beta <- gsw_alpha_on_beta(SA,CT,p)
+#' expect_equal(alpha_on_beta, c(0.452468543022009, 0.449601695030057, 0.387140203094424,
+#'                               0.230778871228268, 0.193747796234162, 0.170946048860385))
 #' @seealso This yields the ratio of the return values from \code{\link{gsw_alpha}} and \code{\link{gsw_beta}}, to within computational precision.
 #' @references
-#' \url{http://www.teos-10.org/pubs/gsw/v3_04/html/gsw_alpha_on_beta.html}
+#' \url{http://www.teos-10.org/pubs/gsw/html/gsw_alpha_on_beta.html}
 gsw_alpha_on_beta <- function(SA, CT, p)
 {
     l <- argfix(list(SA=SA, CT=CT, p=p))
