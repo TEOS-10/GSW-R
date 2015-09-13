44bbea151ff9edf35f5b225691f4b02044a3eea2
Dan Kelley <kelley.dan@gmail.com>
2015-09-13 08:11:06 -0300

    gsw_SP_from_C() updated

--- R/gsw.R
+++ R/gsw.R
@@ -1623,10 +1623,16 @@ gsw_specvol_t_exact  <- function(SA, t, p)
 #' @param p sea pressure [ dbar ]
 #' @return Practical Salinity (PSS-78) [ unitless ]
 #' @examples 
-#' gsw_SP_from_C(34.5487, 28.7856, 10) # 20.009869599086951
+#' library(testthat)
+#' C <- c(34.5487, 34.7275, 34.8605, 34.6810, 34.5680, 34.5600)
+#' t <- c(28.7856, 28.4329, 22.8103, 10.2600,  6.8863,  4.4036)
+#' p <- c(     10,      50,     125,     250,     600,    1000)
+#' SP <- gsw_SP_from_C(C,t,p)
+#' expect_equal(SP,  c(20.009869599086951, 20.265511864874270, 22.981513062527689,
+#'                     31.204503263727982, 34.032315787432829, 36.400308494388170))
 #' @seealso \code{\link{gsw_C_from_SP}} does the reverse.
 #' @references
-#' \url{http://www.teos-10.org/pubs/gsw/v3_04/html/gsw_SP_from_C.html}
+#' \url{http://www.teos-10.org/pubs/gsw/html/gsw_SP_from_C.html}
 gsw_SP_from_C <- function(C, t, p)
 {
     l <- argfix(list(C=C, t=t, p=p))
