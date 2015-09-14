af5ba20b6db1ab8e7429cefa3da154041013d0db
Dan Kelley <kelley.dan@gmail.com>
2015-09-14 07:10:17 -0300

    gsw_latentheat_melting() check values updated (thanks, PB)

--- tests/testthat/test_gsw.R
+++ tests/testthat/test_gsw.R
@@ -197,10 +197,8 @@ test_that("gsw_latentheat_evap_melting", {
           SA <- c(34.7118, 34.8915, 35.0256, 34.8472, 34.7366, 34.7324)
           p <- c(      10,      50,     125,     250,     600,    1000)
           lh <- gsw_latentheat_melting(SA, p)
-          expect_equal(lh, 1e5*c(3.299495187300804, 3.298611954422526, 3.297124383647952,
-                                 3.294972884747496, 3.288480015369891, 3.280715953443947),
-                       scale=1e5, tolerance=1e-6)})
-## FIXME: above, why don't scale=NULL and tolerance=.Machine$double.eps^0.5 work?
+          expect_equal(lh, 1e5*c(3.299496680271213, 3.298613352397986, 3.297125622834541,
+                                 3.294973895330757, 3.288480445559747, 3.280715862416388))})
 
 test_that("gsw_Nsquared", {
           SA <- c(34.7118, 34.8915, 35.0256, 34.8472, 34.7366, 34.7324)
