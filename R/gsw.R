d38a13620486e4baf17274ce28e391c941e6d3eb
Dan Kelley <kelley.dan@gmail.com>
2017-03-12 15:17:31 -0300

    gsw_chem_potential_water_t_exact added

--- R/gsw.R
+++ R/gsw.R
@@ -499,6 +499,7 @@ gsw_C_from_SP <- function(SP, t, p)
 #' pot <- gsw_chem_potential_water_ice(t, p)
 #' expect_equal(pot/1e4, c(-1.340648365149857, -1.644921413491445, -1.480991678890353,
 #'                       -1.272436055728805, -0.711509477199393, 0.045575390357792))
+#' @family things related to chemical potential
 #' @family things related to ice
 #' @references
 #' \url{http://www.teos-10.org/pubs/gsw/html/gsw_chem_potential_water_ice.html}
@@ -511,6 +512,35 @@ gsw_chem_potential_water_ice <- function(t, p)
             rval=double(n), NAOK=TRUE, PACKAGE="gsw")$rval
     if (is.matrix(t))
         dim(rval) <- dim(t)
+    rval
+}
+
+#' Chemical Potential of Water in Seawater
+#'
+#' @template SAtemplate
+#' @template ttemplate
+#' @template ptemplate
+#' @return chemical potential [ J/kg ]
+#' @examples 
+#' library(testthat)
+#' SA <- c(34.7118, 34.8915, 35.0256, 34.8472, 34.7366, 34.7324)
+#' t <- c(28.7856, 28.4329, 22.8103, 10.2600,  6.8863,  4.4036)
+#' p <- c(      10,       50,      125,      250,      600,      1000)
+#' pot <- gsw_chem_potential_water_t_exact(SA, t, p)
+#' expect_equal(pot, c(-8.545561146284534, -8.008085548342105, -5.103980139874876,
+#'                   -0.634067782745442, 3.335566803473286, 7.555434445971858))
+#' @family things related to chemical potential
+#' @references
+#' \url{http://www.teos-10.org/pubs/gsw/html/gsw_chem_potential_water_t_exact.html}
+gsw_chem_potential_water_t_exact <- function(SA, t, p)
+{
+    l <- argfix(list(SA=SA, t=t, p=p))
+    n <- length(l[[1]])
+    rval <- .C("wrap_gsw_chem_potential_water_t_exact",
+            SA=as.double(l$SA), t=as.double(l$t), p=as.double(l$p), n=as.integer(n),
+            rval=double(n), NAOK=TRUE, PACKAGE="gsw")$rval
+    if (is.matrix(SA))
+        dim(rval) <- dim(SA)
     rval
 }
 
