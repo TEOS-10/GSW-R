010402f5fe079cdec1886e3b268b8f5ae3b78193
dankelley <kelley.dan@gmail.com>
2019-03-31 07:30:26 -0300

    gsw_SA_from_Sstar doc: rename first arg in example
    This closes issue #42.

--- R/gsw.R
+++ R/gsw.R
@@ -4389,11 +4389,11 @@ gsw_SA_from_SP_Baltic <- function(SP, longitude, latitude)
 #' @template latitudetemplate
 #' @return Absolute Salinity [ g/kg ]
 #' @examples
-#' SP <- c(34.7115, 34.8912, 35.0247, 34.8436, 34.7291, 34.7197)
+#' Sstar <- c(34.7115, 34.8912, 35.0247, 34.8436, 34.7291, 34.7197)
 #' p <- c(      10,      50,     125,     250,     600,    1000)
 #' lat <- c(     4,       4,       4,       4,       4,       4)
 #' long <- c(  188,     188,     188,     188,     188,     188)
-#' SA <- gsw_SA_from_Sstar(SP, p, long, lat)
+#' SA <- gsw_SA_from_Sstar(Sstar, p, long, lat)
 #' expect_equal(SA, c(34.711724663585905, 34.891561223296009, 35.025594598699882,
 #'                    34.847235885385913, 34.736694493054166, 34.732387111902753))
 #' @family things related to salinity
@@ -4719,7 +4719,6 @@ gsw_specvol  <- function(SA, CT, p)
     1 / gsw_rho(SA, CT, p)
 }
 
-##====
 #' Specific Volume, alpha, and beta
 #'
 #' @template teos10template
